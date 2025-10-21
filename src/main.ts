3203fe45107b514fc9d814964d58b2ea242c7e8f
nigussolomon <nigussolomon444@gmail.com>
2025-10-21 21:32:54 +0300

    implemented db configuration and added a log in main to confirm db connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const dataSource = app.get(DataSource);
+  try {
+    await dataSource.query('SELECT NOW()');
+    console.log('✅ Database connected successfully!');
+  } catch (err) {
+    console.error('❌ Database connection failed:', (err as Error).message);
+  }
+
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
