2f8756f3db5bf7f52eb4164e9e89aee776dd82a4
nigussolomon <nigussolomon444@gmail.com>
2025-10-21 22:00:52 +0300

    added a data source file for typeorm connection and implmented the first role migration

---
+++ data-source.ts
@@ -0,0 +1,16 @@
+// src/data-source.ts
+import { DataSource } from 'typeorm';
+import 'dotenv/config';
+import { Role } from './src/roles/roles.entity';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  database: process.env.POSTGRES_DB || 'postgres',
+  entities: [Role],
+  migrations: ['./migrations/*.ts'],
+  synchronize: false,
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { AppService } from './app.service';
       autoLoadEntities: true,
       synchronize: process.env.NODE_ENV !== 'production',
     }),
+    RolesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
