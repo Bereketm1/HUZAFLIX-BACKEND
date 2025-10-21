517fff372d12a3c61afa85c0ae1fd565e1dbaec4
nigussolomon <nigussolomon444@gmail.com>
2025-10-22 00:15:17 +0300

    added routes for auth service functions

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { type RegisterDto } from './dto/register.dto';
+import { type LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() dto: RegisterDto): Promise<{ message: string }> {
+    return this.authService.register(dto);
+  }
+
+  @Post('login')
+  async login(
+    @Body() dto: LoginDto,
+  ): Promise<{ access_token: Promise<string> }> {
+    return this.authService.login(dto);
+  }
+}
