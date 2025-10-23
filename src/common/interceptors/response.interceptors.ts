ddc48027f166852a2bc5c074d38a41b31c59e831
nigussolomon <nigussolomon444@gmail.com>
2025-10-23 12:49:22 +0300

    added a global response parser

---
+++ src/common/interceptors/response.interceptors.ts
@@ -0,0 +1,68 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApiResponse } from '../dto/response.dto';
+import { Response } from 'express';
+
+@Injectable()
+export class ResponseInterceptor
+  implements NestInterceptor<unknown, ApiResponse>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<unknown>,
+  ): Observable<ApiResponse> {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    function isObjectWithMessage(
+      obj: unknown,
+    ): obj is { message: string; data?: Record<string, any> } {
+      return (
+        typeof obj === 'object' &&
+        obj !== null &&
+        'message' in obj &&
+        typeof (obj as { message: string }).message === 'string'
+      );
+    }
+
+    return next.handle().pipe(
+      map((data: unknown): ApiResponse => {
+        const statusCode = response.statusCode ?? 200;
+
+        if (
+          typeof data === 'object' &&
+          data !== null &&
+          'success' in data &&
+          'status' in data
+        ) {
+          return data as ApiResponse;
+        }
+
+        let message = 'Request successful';
+        let responseData: Record<string, any> | null = null;
+
+        if (isObjectWithMessage(data)) {
+          message = data.message;
+          responseData = data.data ?? null;
+        } else if (typeof data === 'object' && data !== null) {
+          responseData = data as Record<string, any>;
+        } else {
+          responseData = { value: data };
+        }
+
+        return new ApiResponse(
+          statusCode >= 200 && statusCode < 300,
+          statusCode,
+          message,
+          responseData || { data: null },
+        );
+      }),
+    );
+  }
+}
