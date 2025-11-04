d6bcba2b041d93b4b4594407bb747c5b2a271b93
nigussolomon <nigussolomon444@gmail.com>
2025-11-04 09:21:30 +0300

    removed dangling typeorm config for analytics service as it doesn't connect to a db

--- apps/analytics-service/src/app.module.ts
+++ apps/analytics-service/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {
@@ -39,16 +38,6 @@ const jwtOptions: JwtModuleOptions = {
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env'],
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-      username: process.env.POSTGRES_USER || 'postgres',
-      password: process.env.POSTGRES_PASSWORD || 'postgres',
-      database: process.env.POSTGRES_DB || 'postgres',
-      autoLoadEntities: true,
-      synchronize: process.env.NODE_ENV !== 'production',
     }),
     ClientsModule.register([
       {
