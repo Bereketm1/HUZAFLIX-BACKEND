ffdcfbb010a29acbbc190fcc65e67c0c4532c56f
Nigus Solomon <78663320+nigussolomon@users.noreply.github.com>
2025-11-24 12:17:49 +0300

    added an mfa mailer service

--- packages/common/src/common.module.ts
+++ packages/common/src/common.module.ts
@@ -3,6 +3,8 @@ import { JwtModule, JwtModuleOptions, JwtService } from '@nestjs/jwt';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { initCommonSingletons } from './internal/singletons';
 import { MinioService } from './services/minio/minio.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { MfaMailerService } from './services/mfa-mailer/mfa-mailer.service';
 
 @Global()
 @Module({})
@@ -18,13 +20,36 @@ export class CommonModule {
       useSSL?: boolean;
       bucketName?: string;
     },
+    mailerOptions?: {
+      host: string;
+      port: number;
+      secure: boolean;
+      auth: {
+        user: string;
+        pass: string;
+      };
+    },
   ): DynamicModule {
-    initCommonSingletons(options, redisOptions, minioOptions);
+    initCommonSingletons(options, redisOptions, minioOptions, mailerOptions);
 
     return {
       module: CommonModule,
       imports: [
         JwtModule.register(options),
+        MailerModule.forRoot({
+          transport: {
+            host: mailerOptions?.host || 'smtp.example.com',
+            port: mailerOptions?.port || 587,
+            secure: mailerOptions?.secure || false,
+            auth: {
+              user: mailerOptions?.auth?.user || 'your_email@example.com',
+              pass: mailerOptions?.auth?.pass || 'your_email_password',
+            },
+          },
+          defaults: {
+            from: '"No Reply" <noreply@example.com>',
+          },
+        }),
         ...(redisOptions
           ? [
               ClientsModule.register([
@@ -42,6 +67,7 @@ export class CommonModule {
       ],
       providers: [
         MinioService,
+        MfaMailerService,
         {
           provide: 'JWT_SERVICE',
           useExisting: JwtService,
@@ -49,7 +75,9 @@ export class CommonModule {
       ],
       exports: [
         JwtModule,
+        MailerModule,
         MinioService,
+        MfaMailerService,
         'JWT_SERVICE',
         ...(redisOptions ? [ClientsModule] : []),
       ],
